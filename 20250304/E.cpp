caae4e54b2788ea0f45489e6a15b5608776904d0
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-04 22:14:02 +0900

    20250304

--- 20250304/E.cpp
+++ 20250304/E.cpp
@@ -9,22 +9,27 @@ int main(void){
     cin >> h >> w;
     vector<vector<int>> a(h, vector<int>(w));
     rep(i,h) rep(j,w) cin >> a[i][j];
-    
-    rep(bit, 1<<(h+w)){
-        rep(j,(h+w)){
-            if(bit & (1<<j)){
-                if(j < h){
-                    rep(i,w){
-                        cout << a[j][i] << " ";
-                    }
-                    cout << endl;
-                }else{
-                    rep(i,h){
-                        cout << a[i][j-h] << " ";
-                    }
-                    cout << endl;
-                }
-            }
+    vector<int> g;
+    rep(i,h-1) g.push_back(0);
+    rep(i,w-1) g.push_back(1);
+    int ans = 0;
+
+    do{
+        int hi = 0, wi = 0;
+        set<int> st;
+        st.insert(a[0][0]);
+        rep(i,h+w-2){
+            if(g[i] == 0){
+                hi++;
+                st.insert(a[hi][wi]);
+            }else{
+                wi++;
+                st.insert(a[hi][wi]);
+            } 
         }
-    }
+        if(st.size() == h+w-1) ans++;
+    }while(next_permutation(g.begin(), g.end()));
+
+    cout << ans << endl;
+    return 0;
 }

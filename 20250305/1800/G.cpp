ac59d942bb50422a8cb78442e22a2891de16b599
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-06 12:50:16 +0900

    20250306

---
+++ 20250305/1800/G.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0; i<(n); i++)
+using ll = long long;
+using P = pair<int,int>;
+
+int main(void){
+    int t;
+    cin >> t;
+    rep(i,t){
+        int n, k, d;
+        cin >> n >> k >> d;
+        
+    }
+}

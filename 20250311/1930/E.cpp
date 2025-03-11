1ec991e13676a3a99aaf9b5da1efac2e50c3e068
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-11 22:48:09 +0900

    20250311

--- 20250311/1930/E.cpp
+++ 20250311/1930/E.cpp
@@ -5,5 +5,24 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll p, q, r, s;
+    cin >> p >> q >> r >> s;
+
+    vector<vector<char>> grid(q-p, vector<char>(s-r,'.'));
+
+    ll p1 = max(1-a, 1-b);
+    ll p2 = min(n-a, n-b);
+    ll p3 = max(1-a,b-n);
+    ll p4 = min(n-a, b-1);
+
+    while(p1 <= p2){
+        if(p <= a+p1 && q > a+p2 && r <= b+p1 && s > b+p2){
+            grid[a+p1-1][b+p1-1] = '#';
+        }else{
+            
+        }
+    }
 
 }
--- 20250311/1930/F.cpp
+++ 20250311/1930/F.cpp
@@ -5,5 +5,20 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    rep(i,n) cin >> a[i];
 
+    sort(a.begin(), a.end());
+    ll ans = 0;
+    ll s = 0, g = n-1;
+    while (k > 0 && s < g){
+        if(a[g] - a[s+1] < a[g-1] - a[s]) s++;
+        else g--;
+        k--;
+    }
+    ans = a[g] - a[s];
+    cout << ans << endl;
+    return 0;
 }

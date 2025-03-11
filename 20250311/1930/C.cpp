be5e44269e6adeab524eee00925af7fc0f3593af
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-11 19:42:22 +0900

    20250311

--- 20250311/1930/C.cpp
+++ 20250311/1930/C.cpp
@@ -5,5 +5,18 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
+    int n;
+    string s;
+    cin >> n >> s;
+
+    rep(l,n-1){
+        int cnt = 0;
+        rep(k,n-l-1){
+            if(s[k] == s[k+l+1]) break;
+            else cnt++;
+        }
+        cout << cnt << endl;
+    }
+    return 0;
 
 }
--- 20250311/1930/D.cpp
+++ 20250311/1930/D.cpp
@@ -5,5 +5,29 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
+    int n, m;
+    cin >> n >> m;
+    vector<string> a(n), b(m);
+    rep(i,n) cin >> a[i];
+    rep(i,m) cin >> b[i];
+
+    rep(i,n-m+1){
+        rep(j,n-m+1){
+            bool ok = true;
+            rep(k,m){
+                rep(l,m){
+                    if(a[i+k][j+l] != b[k][l]){
+                        ok = false;
+                        break;
+                    }
+                }
+            }
+            if(ok){
+                cout << i+1 << ' ' << j+1 << endl;
+                return 0;
+            }
+        }
+    }
+    return 0;
 
 }

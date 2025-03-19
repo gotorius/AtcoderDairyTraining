d845bbd3cd87fda44a9252d64eccee5f053ef574
gotorius <ryosuke1108g@yahoo.ne.jp>
2025-03-19 20:03:42 +0900

    20250319

--- 20250319/2000/C.cpp
+++ 20250319/2000/C.cpp
@@ -5,6 +5,24 @@ using ll = long long;
 using P = pair<int,int>;
 
 int main(void){
-    
+    int n, m;
+    cin >> n >> m;
+    vector<string> s(n);
+    rep(i,n) cin >> s[i];
 
+    int ans = 0;
+    rep(i,n){
+        for(int j = i+1; j < n; j++){
+            bool ok = true;
+            rep(k,m){
+                if(s[i][k] == 'x' && s[j][k] == 'x'){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
